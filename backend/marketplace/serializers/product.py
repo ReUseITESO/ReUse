3b2b6213ad4b02126aa12531472e508220013bcd
victortelles <victor-tah@outlook.com>
2026-02-19 22:12:09 -0600

    feat(marketplace): add product serializer

---
+++ backend/marketplace/serializers/product.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from marketplace.models import Products
+from marketplace.serializers.category import CategorySerializer
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    """ Serializador para la lista de productos (Obj -> JSON). """
+    category = CategorySerializer(read_only=True)
+    seller_name = serializers.CharField(
+        source='seller.name', read_only=True
+    )
+
+    class Meta:
+        model = Products
+        fields = [
+            "id",
+            "title",
+            "description",
+            "condition",
+            "transaction_type",
+            "status",
+            "price",
+            "image_url",
+            "category",
+            "seller_name",
+            "created_at",
+        ]
