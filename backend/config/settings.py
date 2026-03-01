d3bd1c751ef0cd78721706d99cd28272849b9179
unknown <isaac.noriega@iteso.mx>
2026-03-01 14:47:38 -0600

    feat(backend): enable gamification app and configure CORS for mock auth
    - Add gamification to INSTALLED_APPS
    - Configure CORS_ALLOW_HEADERS to include x-mock-user-id
    - Enable mock authentication header in development

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,8 +30,7 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "core",
     "marketplace",
-    # TODO: uncomment when gamification app has models and migrations ready
-    # "gamification",
+    "gamification",
 ]
 
 MIDDLEWARE = [
@@ -190,3 +189,16 @@ CORS_ALLOWED_ORIGINS = os.environ.get(
 ).split(",")
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "x-mock-user-id",  # For mock authentication in development
+]
