f534a375921681242edfa5d68988df2f0ad8cf36
LuisArturo21 <arturo.gutierrez@iteso.mx>
2026-03-03 11:47:33 -0600

    feat(gamification): add Badge and UserBadge models

--- backend/gamification/models/__init__.py
+++ backend/gamification/models/__init__.py
@@ -2,8 +2,8 @@
 # These models are defined in this directory as reference implementations
 # based on docs/database/erd_v1.md. The team can modify them as needed.
 #
-# from .badges import Badges
-# from .user_badges import UserBadges
-# from .environment_impact import EnvironmentImpact
+from .badges import Badges
+from .user_badges import UserBadges
+from .environment_impact import EnvironmentImpact
 #
-# __all__ = ["Badges", "UserBadges", "EnvironmentImpact"]
+__all__ = ["Badges", "UserBadges", "EnvironmentImpact"]
