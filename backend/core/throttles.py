3b9177bf3ccb6093099c216c697047d638e07885
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2026-04-07 08:56:06 -0600

    Added rate limiting to user, email, auth, and anon endpoints (#130)

---
+++ backend/core/throttles.py
@@ -0,0 +1,31 @@
+from rest_framework.throttling import AnonRateThrottle, SimpleRateThrottle, UserRateThrottle
+
+
+class AuthRateThrottle(SimpleRateThrottle):
+    scope = "auth"
+
+    def get_cache_key(self, request, view):
+        ident = (
+            request.META.get("HTTP_X_FORWARDED_FOR", "").split(",")[0].strip()
+            or request.META.get("REMOTE_ADDR", "")
+        )
+        return self.cache_format % {"scope": self.scope, "ident": ident}
+
+
+class EmailVerificationRateThrottle(SimpleRateThrottle):
+    scope = "email_verification"
+
+    def get_cache_key(self, request, view):
+        ident = (
+            request.META.get("HTTP_X_FORWARDED_FOR", "").split(",")[0].strip()
+            or request.META.get("REMOTE_ADDR", "")
+        )
+        return self.cache_format % {"scope": self.scope, "ident": ident}
+
+
+class StandardAnonThrottle(AnonRateThrottle):
+    scope = "anon"
+
+
+class StandardUserThrottle(UserRateThrottle):
+    scope = "user"
